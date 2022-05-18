a43b10412d3ffaae4db6e94e2d6573abf8964068
Rümeysa Ceylan <59581456+RumeysaCeylan@users.noreply.github.com>
2022-05-18 14:10:15 +0300

    Update HostnameController.php

--- eklenti/app/Controllers/HostnameController.php
+++ eklenti/app/Controllers/HostnameController.php
@@ -8,34 +8,6 @@ class HostnameController
 	public function get()
 	{
 
-		/////////////////////////////////////////////////
-
-
-		$command = Command::runSudo("ls -lah /home/liman");
-		$files = explode("\n", $command);
-		array_splice($files, 0, 1);
-		
-		foreach ($files as &$file) {
-			$file = preg_replace('/\s+/', ' ', $file);
-			$file = explode(" ", $file);
-			$file = [
-				"permissions" => $file[0],
-				"user" => $file[2],
-				"group" => $file[3],
-				"size" => $file[4],
-				"date" => $file[5] . " " . $file[6] . " " . $file[7],
-				"name" => $file[8]
-			];
-		}
-		
-		return view("table", [
-			"value" => $files,
-			"title" => ["İsim", "İzinler", "Sahip", "Grup", "Dosya Boyutu", "Son Değiştirilme"],
-			"display" => ["name", "permissions", "user", "group", "size", "date"]
-		]);
-
-
-		/////////////////////////////////////////////
 		$output = Command::run('whoami');
 		return respond($output, 200);
 	}
